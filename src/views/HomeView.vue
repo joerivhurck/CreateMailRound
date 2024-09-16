c1d8bb2214960ef676eb5439df7dc88a8253f173
joeri <joerivanhurck@hotmail.be>
2024-09-16 12:23:41 +0200

    start

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+const streets = {
+  name : 'asbronstraat',
+  number : ["1","2"]
+}
+</script>
+
+<template>
+  <div>{{ streets.name }}</div>
+  <div v-for="number in streets.number" :key="number">{{ number }}</div>
+</template>
